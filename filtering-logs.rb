250588f93029947499c01f96f651f3130bac5fc3
Masao Takaku <tmasao@acm.org>
2018-02-12 08:50:03 +0900

    class AccessLog

--- filtering-logs.rb
+++ filtering-logs.rb
@@ -3,39 +3,32 @@
 require "uri"
 require "device_detector"
 
+require_relative "util.rb"
+
 if $0 == __FILE__
-  ACCESS_LOG_REGEXP = /\A([0-9\.]+) (\S+) (\S+) \[([^\]]+)\] "([^"]+)" ([0-9]+) ([\-0-9]+) "([^"]*)" "([^"]*)"\z/o
-  REQUEST_REGEXP = /\A(\S+) (.*) HTTP\/(0\.9|1\.0|1\.1|2\.0)\z/o
-  SUFFIX_REGEXP = /\.(css|jpe?g|png|gif|ico|js|mso|xml|rss|rdf)/o
-  PATH_REGEXP = /\A\/(favicon.ico|robots.txt)\z/o
-  TULIPS_PATH_REGEXP = /\A(\/|\/lib\/)\z/o
+  include AccessLog
   count = Hash.new(0)
   ARGF.each do |line|
-    if ACCESS_LOG_REGEXP =~ line.chomp
-      dummy, ip_addr, user, group, time, request, status, size, referer, agent, = $~.to_a
-      client = DeviceDetector.new(agent)
+    log = parse_line(line)
+    if log[:valid]
+      client = DeviceDetector.new(log[:agent])
       if client.bot?
         count[:bot] += 1
         next
       end
-      if REQUEST_REGEXP =~ request
-        dummy, method, path, = $~.to_a
-        begin
-          uri = URI.parse(path)
-          if uri.path =~ PATH_REGEXP or uri.path =~ SUFFIX_REGEXP or uri.path =~ TULIPS_PATH_REGEXP
-            #p path
-            count[:request] += 1
-            next
-          end
-        rescue URI::InvalidURIError
+      begin
+        uri = URI.parse(log[:path])
+        if uri.path =~ PATH_REGEXP or uri.path =~ SUFFIX_REGEXP or uri.path =~ TULIPS_PATH_REGEXP
+          #p path
           count[:request] += 1
           next
         end
-        count[:valid] += 1
-        puts line
-      else
-        count[:other] += 1
+      rescue URI::InvalidURIError
+        count[:request] += 1
+        next
       end
+      count[:valid] += 1
+      puts line
     else
       count[:other] += 1
     end
---
+++ util.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+require "uri"
+require "device_detector"
+
+module AccessLog
+  SUFFIX_REGEXP = /\.(css|jpe?g|png|gif|ico|js|mso|xml|rss|rdf)/o
+  PATH_REGEXP = /\A\/(favicon.ico|robots.txt)\z/o
+  TULIPS_PATH_REGEXP = /\A(\/|\/lib\/)\z/o
+  ACCESS_LOG_REGEXP = /\A([0-9\.]+) (\S+) (\S+) \[([^\]]+)\] "([^"]+)" ([0-9]+) ([\-0-9]+) "([^"]*)" "([^"]*)"\z/o
+  REQUEST_REGEXP = /\A(\S+) (.*) HTTP\/(0\.9|1\.0|1\.1|2\.0)\z/o
+  def parse_line(line)
+    result = {}
+    if ACCESS_LOG_REGEXP =~ line.chomp
+      valid = true
+      dummy, ip_addr, user, group, time, request, status, size, referer, agent, = $~.to_a
+      if REQUEST_REGEXP =~ request
+        dummy, method, path, protocol, = $~.to_a
+      end
+    end
+    {
+      ip_addr: ip_addr,
+      user: user,
+      group: group,
+      time: time,
+      request: request,
+      status: status,
+      size: size,
+      referer: referer,
+      agent: agent,
+      method: method,
+      path: path,
+      protocol:protocol,
+      valid: valid,
+    }
+  end
+end
