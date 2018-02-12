45ac13da9f619e1edea04ba26d09b56e76b4d97a
Masao Takaku <tmasao@acm.org>
2018-02-12 12:15:03 +0900

    check expiration time (default: 2400 sec.)

--- ext-session.rb
+++ ext-session.rb
@@ -3,6 +3,8 @@
 require "json"
 require_relative "util.rb"
 
+SESSION_EXPIRE = 60 * 60
+
 if $0 == __FILE__
   include AccessLog
   sessions = {}
@@ -10,9 +12,21 @@ if $0 == __FILE__
     log = parse_line(line)
     if log[:valid]
       key = [log[:ip_addr], log[:agent]]
+      time = parse_time(log[:time])
+      if sessions[key]
+        last_time = parse_time(sessions[key].last[:time])
+        if time - last_time > SESSION_EXPIRE
+          #p [ time, last_time ]
+          if sessions[key].find{|e| e[:path] =~ /bibid=\d+/o or e[:path] =~ /(ncid|isbn|issn)=\w+/o }
+            sessions[key].each do |e|
+              puts [ key.hash, e.to_json ].join("\t")
+            end
+          end
+          sessions.delete key
+        end
+      end
       sessions[key] ||= []
       sessions[key] << log
-      #pp sessions[key] if sessions[key].size == 10
     end
   end
   sessions.keys.each do |key|
