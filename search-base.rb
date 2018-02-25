8008047a2ac75a639e2440e7b1a1516768e8087a
Masao Takaku <tmasao@acm.org>
2018-02-25 09:32:53 +0900

    different weights for baseline.

--- search-base.rb
+++ search-base.rb
@@ -11,20 +11,22 @@ if $0 == __FILE__
     topic_id, query, = line.chomp.split(/\t/)
     topic_dir = File.join(BASEDIR, topic_id)
     FileUtils.mkdir(topic_dir) if not File.exist? topic_dir
-    filename = File.join(topic_dir, "#{RUN}.res")
-    params = {
-      q: query,
-      defType: "edismax",
-      qf: "title^10.0 default^1.0",
-      rows: 1000,
-      fl: "*,score",
-      sort: "score desc, date desc",
-    }
-    response = solr.get("select", params: params)
-    docs = response["response"]["docs"]
-    open(filename, "w") do |io|
-      docs.each do |doc|
-        io.puts [ doc["id"], doc["score"], doc["title"], doc["author"], doc["date"] ].join("\t")
+    [ 1, 2, 5, 10, 20 ].each do |weight|
+      filename = File.join(topic_dir, "#{RUN}#{ "%02d" % weight }.res")
+      params = {
+        q: query,
+        defType: "edismax",
+        qf: "title^#{weight}.0 default^1.0",
+        rows: 1000,
+        fl: "*,score",
+        sort: "score desc, date desc",
+      }
+      response = solr.get("select", params: params)
+      docs = response["response"]["docs"]
+      open(filename, "w") do |io|
+        docs.each do |doc|
+          io.puts [ doc["id"], doc["score"], doc["title"], doc["author"], doc["date"] ].join("\t")
+        end
       end
     end
   end
